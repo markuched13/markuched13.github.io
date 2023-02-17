e8716aa752eed4f9fd773dbf2b3082439a103253
Mark Uche <113513376+markuched13@users.noreply.github.com>
2023-02-17 03:25:51 +0100

    Update solve.py

--- solvescript/echoctf/doorknob/solve.py
+++ solvescript/echoctf/doorknob/solve.py
@@ -5,12 +5,12 @@ io = remote('10.0.30.92', 5903)
 # Get menu option
 io.recvuntil('menu: Option ')
 menu = io.recvline().strip().decode()
-print("Menu option found: " +menu)
+info("Menu option found: %#x", menu)
 
 # Get submenu option
 io.recvuntil('submenu: Sub Option')
 submenu = io.recvline().strip().decode()
-print("Submenu option found: " +submenu)
+info("Submenu option found: %#x", submenu)
 
 # Send the corresponding menu & submenu options
 io.recvuntil('Choose option or press enter to reprint:')
