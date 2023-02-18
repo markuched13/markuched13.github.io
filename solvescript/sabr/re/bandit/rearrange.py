f8616d9ca2360b932c80f84c4e40ac2b61c965c9
Mark Uche <113513376+markuched13@users.noreply.github.com>
2023-02-18 22:53:33 +0100

    Create rearrange.py

---
+++ solvescript/sabr/re/bandit/rearrange.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+string = 'neg{eonfnqanc_ufhp_ren_frg}'
+
+char_list = list(string)
+
+print(char_list)
+
+# After manipulating the string form i got the flag
+# b'sabr{trash_pandas_are_cute}'
