7c4e03edbe7a2bd9a0ae76a69f77d48f098e8fc6
Mark Uche <113513376+markuched13@users.noreply.github.com>
2023-02-19 23:18:24 +0100

    Update solve.py

--- solvescript/echoctf/doorknob/solve.py
+++ solvescript/echoctf/doorknob/solve.py
@@ -1,6 +1,8 @@
 from pwn import *
 
 io = remote('10.0.30.92', 5903)
+warnings.filterwarnings("ignore", category=BytesWarning, message="Text is not bytes; assuming ASCII, no guarantees.")
+warnings.filterwarnings("ignore")
 
 # Get menu option
 io.recvuntil('menu: Option ')
