db4fc8880f006ebff313b111753d51de9f9d6bee
Mark Uche <113513376+markuched13@users.noreply.github.com>
2023-05-10 00:22:17 +0100

    Create Decode.py

---
+++ solvescript/thm/Anonymous_Playground/Decode.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# Author: Hack.You
+
+word = 'hEzAdCfHzA::hEzAdCfHzAhAiJzAeIaDjBcBhHgAzAfHfN'
+cipher = word.replace(':', '')
+alphabets = []
+decode = []
+
+for _ in range(ord('a'), ord('z')+1):
+    alphabets.append(chr(_))
+
+for i in range(0, len(cipher), 2):
+    first_ = cipher[i]
+    second_ = cipher[i+1].lower()
+
+    first_position = ord(first_) - ord('a') + 1
+    second_position = ord(second_) - ord('a') + 1
+
+    decoded_position = (first_position + second_position) % 26 
+    decoded = alphabets[decoded_position - 1]
+    decode.append(decoded)
+
+print(f'Encoded text: {cipher}')
+print(f'Alphabet list: {"".join(alphabets)}')
+print(f"Decoded: {''.join(decode)[:5]}::{''.join(decode)[10:]}")
+
+
+
